9b442aaf2c81bc7e64e2eb458244caf1195f67fd
Mrsaugatshah <saugats082@gmail.com>
2026-07-11 16:19:50 +0545

    Menu items config and Menu display

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("tables/", views.tables_views, name="tables_view_url")
+    path("tables/", views.tables_views, name="tables_view_url"),
+    path("menu<table_id>/", views.menu_views, name="menu_view_url"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from .decorators import role_required
 from accounts.models import User
-from .models import Table
+from .models import Table,Category
 
 
 @role_required([User.Role.WAITER])
 def tables_views(request):
-    tables = Table.objects.all()
+   
+    tables=Table.objects.all()
     return render(request, "orders/tables.html", {"tables": tables})
+
+
+
+@role_required([User.Role.WAITER])
+def menu_views(request,table_id):
+   
+     categories=Category.objects.all()
+     return render(request, "orders/menu.html", {"categories": categories,'table_id':table_id})
+
