9a4189cb3a48eb327627dfe53f74d932a78017c9
Mrsaugatshah <saugats082@gmail.com>
2026-07-12 21:33:51 +0545

    feat: to implement place order logic in db

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .decorators import role_required
 from accounts.models import User
-from .models import Table,Category
+from .models import Table,Category,Order,OrderItem,MenuItem
+import json
+from django.contrib import messages
 
 
 @role_required([User.Role.WAITER])
@@ -13,8 +15,51 @@ def tables_views(request):
 
 
 @role_required([User.Role.WAITER])
-def menu_views(request,table_id):
-   
-     categories=Category.objects.all()
-     return render(request, "orders/menu.html", {"categories": categories,'table_id':table_id})
+def menu_views(request, table_id):
+
+    if request.method == "POST":
+
+        data = request.POST.get("order_data")
+        data_list = json.loads(data)
+
+        # Get table from URL parameter
+        table_obj = Table.objects.get(pk=table_id)
+
+        # Create order
+        order = Order.objects.create(
+            table=table_obj
+        )
+
+        # Create order items
+        for orderitem in data_list:
+
+            menu_item = MenuItem.objects.get(
+                pk=orderitem.get("id")
+            )
+
+            OrderItem.objects.create(
+                order=order,
+                menu_item=menu_item,
+                price=menu_item.price,
+                quantity=orderitem.get("qty")
+            )
+
+        messages.success(
+            request,
+            "Order created successfully"
+        )
+
+        return redirect("tables_view_url")
+
+
+    categories = Category.objects.all()
+
+    return render(
+        request,
+        "orders/menu.html",
+        {
+            "categories": categories,
+            "table_id": table_id
+        }
+    )
 
